d5ebfeca4f392d78d36371732a1967af37909d02
AaronMoroneyDesignCo <102585645+AaronMoroneyDesignCo@users.noreply.github.com>
2022-07-08 19:14:19 +0100

    Cart function after mentor session
    Debugged for cart function to now receive information from the quantity and colors. follow comments on each line to find the logic of each item.
    
    106 - 117

--- front/js/productFront.js
+++ front/js/productFront.js
@@ -12,6 +12,7 @@ let splitForId = queryString.slice(5);
 console.log(splitForId);
 
 const newUri = 'http://localhost:3000/api/products/' + splitForId;
+let product;
 
 //fetch the new URI from get request
 fetch(newUri) 
@@ -20,6 +21,7 @@ fetch(newUri)
         console.log(data)
         createIndividualProductView(data);
         populateDropdown(data);
+        product = data;
 }); //gives back an individual object
 
 // dont need iterate over object to get keys / values -
@@ -44,21 +46,20 @@ function createIndividualProductView(data) {
     individualProductDescription.innerText = data.description;
     individualProductImg.src = data.imageUrl;
 
-    
-    
     //append
     imgParent.appendChild(individualProductImg);
 
     return createIndividualProductView;
 }
 
-//function for populating the dropdown logic - decomposition into smallest parts
-//I have the array I want to access nested within the object 
-//Access the array
-//iterate over the array
-//Access the dom 
-//populate the dom 
-//append to dom
+/*function for populating the dropdown logic - decomposition into smallest parts
+I have the array I want to access nested within the object 
+Access the array
+iterate over the array
+Access the dom 
+populate the dom 
+append to dom
+*/
 
 const populateDropdown = (data) => {
     //access select node
@@ -76,14 +77,51 @@ const populateDropdown = (data) => {
     }
 }
 
+/*
+** | from openClassRooms 
+*/
+
+/* cart can be made as an array containing three things
+- product id, quanitity, color
+use localStroage to access this array
+when you add a product to the cart, add a new element to the array
+when you add the exact same product, increase the quantity 
+*/
 
 
+//check, if product is already in array using if / else
 
 
+function cartButtonPush(data) {
+  //get access to button + quantity + options
+  
+  
+
+    
+
+    
+}
+
+  //console.log(localStorage.getItem('productChoice')); 
+var button = document.getElementById('addToCart');  //console.log(button); working
+button.addEventListener('click', () => { 
+    var option = document.getElementById('colorsParent').value;
+    var quantity = document.getElementById('quantity').value; 
+    var cart = {_id: product, color: option, quantity: quantity };
+    console.log(cart); //undefined option and quantity
+    let shoppingCart = localStorage.getItem('cart') || []; //
+    //console.log(localStorage.setItem('cart', JSON.stringify(cart))); //push to something that isnt
+    shoppingCart.push(cart);
+    console.log(shoppingCart);
+    console.log(product._id);
+});
 
 
+  
 
-//onClickEvent - button, add to cart.
-//-----no duplicates? if else statement / switch
-//-----keep local storage syned to the cart, if else
 
+      //push select data + quantity data
+    /*if (data._id === data._id && data.option === data.option) {
+       console.log(data.quantity++);
+    };
+    */
