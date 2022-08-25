c6797af8191a534b6ac09fd719031a2e7141f163
AaronMoroneyDesignCo <102585645+AaronMoroneyDesignCo@users.noreply.github.com>
2022-08-25 15:06:11 +0100

    Error with query string
    Needed to capitalize CONST

--- code/front/js/confirmation.js
+++ code/front/js/confirmation.js
@@ -6,13 +6,11 @@
 //populate the conformation tab with it 
 
 //get query string
-
 const QUERY_STRING = window.location.search;
-console.log(queryString);
-let confirmationNumber = queryString.slice(2);
+console.log(QUERY_STRING);
+let confirmationNumber = QUERY_STRING.slice(2);
 console.log(confirmationNumber);
 
 //dom access
-
 let orderId = document.getElementById('orderId');
 orderId.innerText = confirmationNumber;
